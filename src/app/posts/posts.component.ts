d787f5df8c6a08b8a79a340f7ff68c962022834e
yair shivek <yair@shivek.co.il>
2021-01-24 09:37:01 +0200

    ok

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from './post';
-//import {HttpClient} from '@angular/common/http';
 import { PostsService } from './posts.service';
 //import { catchError, map, tap } from 'rxjs/operators';
 
@@ -20,21 +19,6 @@ export class PostsComponent implements OnInit {
   ngOnInit(): void {
 
     this.greet = this.postsService.sayHello("moshe");
-
-
-    // // promis
-    // x.then(success, error, notify);
-
-    // // observer
-    // x.subscribe(success, error, complete);
-
-
-    //  this.http.get("https://jsonplaceholder.typicode.com/posts").subscribe(resp =>{
-    //   console.log(resp);
-    //   this.posts = resp;
-    // })
-
-    //this.posts = this.postsService.getPosts();
 
     this.postsService.getPosts().subscribe((posts:Post[]) =>  {
       console.log(posts);
